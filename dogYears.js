733882df2be336f1ece41ccda4cdccf7aae28b16
Catron-Gainey <catrong23@gmail.com>
2024-02-05 06:34:00 -0500

    first commit

---
+++ dogYears.js
@@ -0,0 +1,20 @@
+// My age
+let myAge = 10;
+// i set early years
+let earlyYears = 2;
+earlyYears = earlyYears*10.5;
+// i set laterYears
+let laterYears = myAge-2;
+// I multiplied it
+laterYears *=4; 
+console.log(earlyYears)
+console.log(laterYears)
+//I added them up
+myAgeInDogYears = earlyYears+laterYears;
+// made it lower case
+let myName = 'Catron' 
+myName = myName.toLowerCase();
+console.log(myName)
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} in dog years.`)
+
+
